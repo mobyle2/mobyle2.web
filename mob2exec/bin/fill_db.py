536b6e1e7d91862311bbe46f1a72830d4944a563
Bertrand Néron <bneron@pasteur.fr>
2012-10-19 18:23:34 +0200

    add several keys ( name, cmd_line, inputs ) in job to simulate a real job

--- mob2exec/bin/fill_db.py
+++ mob2exec/bin/fill_db.py
@@ -2,19 +2,49 @@ import sys
 sys.path.append( '/home/bneron/Mobyle/Mobyle2_moteur' )
 import cPickle
 import time 
+import random
 
 from  lib.core.status import Status 
+
+cmdlines={
+		1:{ 'name':'blast', 
+		     'cmd_line': 'blastall -p blastp -d uniprot_sprot -i abcd2_mouse.fa -e 0.1 -o blast2.txt',
+		     'inputs':[( 'abcd2_mouse.fa', """>ABCD2_MOUSE RecName: Full=ATP-binding cassette sub-family D member 2; AltName: Full=Adrenoleukodystrophy-related protein;
+MIHMLNAAAYRVKWTRSGAAKRAACLVAAAYALKTLYPIIGKRLKQPGHRKAKAEAYSPAENREILHCTEIICKKPAPGL
+NAAFFKQLLELRKILFPKLVTTETGWLCLHSVALISRTFLSIYVAGLDGKIVKSIVEKKPRTFIIKLIKWLMIAIPATFV"""
+		      )]
+		     },
+		2:{ 'name': 'golden', 
+		    'cmd_line': 'golden uniprot_sprot:abcd2_rat',
+		    'inputs':[]
+		    },
+		3:{ 'name': 'clustalw',
+		    'cmd_line':'clustalw -align -infile=abcd.fasta',
+		    'inputs' : [( 'abcd.fasta' , """>1pgb
+MTYKLILNGKTLKGEAVDAATAEKVFKQYANDNGVDGEWTYTKTFTVTE
+>1txt
+MTYKLILNGKTLKGETTTEAVDAATAEKVNDNGVDGEWTYDDATKTFTVTE
+>1trt
+MTYKLILNGKTLKGETTTEAVDAATAEKVFKQYANDNGVDGEWTYDDATKTFTVTE""")]
+		    }
+		}
 
 db ={}
 job_id = 0
 for job_id in range(0,500):
-	db[job_id] = {'id' : job_id ,
-                      'create_time': time.time(), 
-                      'end_time': -1 ,
-                      'status': Status( Status.BUILDING ) ,
-                      'has_been_notified' : False ,
-                      'owner' : 'a_user_space',
-                   }
+	j = cmdlines[ random.randint(1,3)]
+	db[job_id] = {
+				  'id' : job_id ,
+				  'job_name' :j['name'],
+                  'create_time': time.time(), 
+                  'end_time': -1 ,
+                  'status': Status( Status.BUILDING ) ,
+                  'cmd_line': j['cmd_line'],
+                  'inputs': j['inputs'] ,
+                  'has_been_notified' : False ,
+                  'owner' : 'a_user_space',
+                  }
+	
 with open('/tmp/mob2.db', 'w') as f:
 	cPickle.dump( db , f )
 	
