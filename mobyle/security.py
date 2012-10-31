816e2d5da07b2d08ad5411a09a4d9da52f5e2ab1
Adrien Saladin <adrien.saladin@gmail.com>
2012-10-31 18:04:50 +0100

    handle case where user has no group

--- mobyle/security.py
+++ mobyle/security.py
@@ -5,5 +5,5 @@ def groupFinder(userid, request):
      #try to find user in database:
      user = request.db.users.find_one({"username": userid})
      if user is not None:
-        print "returning ",  user['groups']
-        return user['groups']
+        return user.get('groups', [])
+
