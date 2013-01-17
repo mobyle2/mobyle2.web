d63e391a1b37142c1375e073b8639eb1996178bc
Hervé Ménager <herve.menager@gmail.com>
2013-01-17 21:34:33 +0100

    active links in navbar
    Added a directive to style a link when it is active (e.g. its path
     is the current angular route) and added it to the index navbar.

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,11 +1,31 @@
 'use strict';
 
 /* Directives */
-
-
 angular.module('myApp.directives', []).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
   }]);
+
+angular.module('myApp.directives', []).
+directive('activeLink', ['$location', function(location) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs, controller) {
+            var clazz = attrs.activeLink;
+            var path = attrs.href;
+            path = path.substring(1); //hack because path does bot return including hashbang
+            scope.location = location;
+            scope.$watch('location.path()', function(newPath) {
+                console.log(newPath+'=='+path);
+                if (path === newPath) {
+                    element.parent().addClass(clazz);
+                } else {
+                    element.parent().removeClass(clazz);
+                }
+            });
+        }
+
+    };
+}]);
