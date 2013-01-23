e92b0bcb3fb2c632737f9d4ac0ee912048c28e5b
Bertrand Néron <bneron@pasteur.fr>
2013-01-23 15:22:49 +0100

    set minimal version in pyramid requirement (>=1.4)
    as with 1.3.2 we got the following error:
     File
     "/usr/lib64/python2.7/site-packages/mobyle.web-0.0-py2.7.egg/mobyle/web/__init__.py",
     line 41, in main
         authentication_policy = AuthTktAuthenticationPolicy('seekrit',
         callback=groupFinder,hashalg='sha512')
         TypeError: __init__() got an unexpected keyword argument 'hashalg'
    
     when the server is start (pserve --reload development.ini)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(here, 'README.md')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
-    'pyramid',
+    'pyramid>=1.4',
     'pyramid_debugtoolbar',
     'waitress',
     "pyramid_beaker",
