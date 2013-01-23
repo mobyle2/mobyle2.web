4de8af7fcf464993ffdbcc934702dd15a7b0a035
Olivier Sallou <olivier.sallou@irisa.fr>
2013-01-23 17:47:24 +0100

    Add statistics rendering page and views

--- mobyle/web/__init__.py
+++ mobyle/web/__init__.py
@@ -110,6 +110,8 @@ def main(global_config, **settings):
     dconfig['templates'] = 'mobyle.web:templates/dashboard.mako'
     Dashboard.add_dashboard([MobyleConfig,User,Project,Job,Program],config)
 
+    config.add_route('statistics', '/admin/stats')
+
     
     return config.make_wsgi_app()
 
