db8087ee2df23f1bb463b34bba18b45a510aaec2
Hervé Ménager <herve.menager@gmail.com>
2013-03-04 18:29:55 +0100

    added angularui for test

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('awa', ['awa.filters', 'awa.services', 'awa.directives', 'ngSanitize']).
+angular.module('awa', ['awa.filters', 'awa.services', 'awa.directives', 'ngSanitize', 'ui']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/services', {templateUrl: 'partials/services.html', controller: ServicesCtrl});
     $routeProvider.when('/services/:serviceId', {templateUrl: 'partials/serviceDetail.html', controller: ServiceDetailCtrl});
