b43366d0035daf807f8122bc11e2d6f4889ca223
Hervé Ménager <hmenager@pasteur.fr>
2013-03-15 18:33:41 +0100

    added recursive directive which should be widely used...

--- app/js/directives.js
+++ app/js/directives.js
@@ -81,3 +81,36 @@ angular.module('awa.directives')
       }
     }
   });
+
+// recursive directive example
+// (from https://groups.google.com/forum/#!topic/angular/vswXTes_FtM)
+module.directive("recursive", function($compile) {
+    return {
+        restrict: "EACM",
+        priority: 100000,
+        compile: function(tElement, tAttr) {
+            var contents = tElement.contents().remove();
+            var compiledContents;
+            return function(scope, iElement, iAttr) {
+                if(!compiledContents) {
+                    compiledContents = $compile(contents);
+                }
+                iElement.append(
+                    compiledContents(scope, 
+                                     function(clone) {
+                                         return clone; }));
+            };
+        }
+    };
+});
+
+module.directive("tree", function() {
+    return {
+        scope: {tree: '='},
+        template: '<p>{{ tree.text }}</p><ul><li ng-repeat="child in tree.children"><recursive><span tree="child"></span></recursive></li></ul>',
+        compile: function() {
+            return  function() {
+            }
+        }
+    };
+});
