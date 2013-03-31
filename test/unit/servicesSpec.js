9e3fa240baa7b23577765bd20f27a9aab077646a
Hervé Ménager <herve.menager@gmail.com>
2013-03-31 22:27:21 +0200

    mbsimple service unit tests
    removed 'version' unit test from the seed and added unit tests
    for the mbsimple service.

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -3,12 +3,24 @@
 /* jasmine specs for services go here */
 
 describe('service', function() {
-  beforeEach(module('myApp.services'));
+    beforeEach(module('awa.services'));
 
 
-  describe('version', function() {
-    it('should return current version', inject(function(version) {
-      expect(version).toEqual('0.1');
-    }));
-  });
+    describe('mbsimple', function() {
+        it('should return true if the parameter has simple property set to true', inject(function(mbsimple) {
+            expect(mbsimple({'simple':true})).toEqual(true);
+        }));
+        it('should return false if the parameter has simple property set to false', inject(function(mbsimple) {
+            expect(mbsimple({'simple':false})).toEqual(false);
+        }));
+        it('should return false if the parameter has simple property unspecified', inject(function(mbsimple) {
+            expect(mbsimple({})).toEqual(false);
+        }));
+        it('should return false if the paragraph contains only non-simple children', inject(function(mbsimple) {
+            expect(mbsimple({'children':[{'simple':false},{'simple':false}]})).toEqual(false);
+        }));
+        it('should return true if the paragraph contains at least one simple child', inject(function(mbsimple) {
+            expect(mbsimple({'children':[{'simple':true},{'simple':false}]})).toEqual(false);
+        }));
+    });
 });
