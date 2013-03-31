b99e920a0f2b003093cbb684b3dd60326055c955
Hervé Ménager <herve.menager@gmail.com>
2013-03-31 22:58:09 +0200

    searchServices unit tests added.

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -3,17 +3,25 @@
 /* jasmine specs for filters go here */
 
 describe('filter', function() {
-  beforeEach(module('myApp.filters'));
+  beforeEach(module('awa.filters'));
 
 
-  describe('interpolate', function() {
-    beforeEach(module(function($provide) {
-      $provide.value('version', 'TEST_VER');
+  describe('searchServices', function() {
+    it('should not filter if no query provided', inject(function(searchServicesFilter) {
+      var services = [{'name':'test1'},{'name':'test2'}];
+      var q = '';
+      expect(searchServicesFilter(services,q)).toEqual(services);
     }));
-
-
-    it('should replace VERSION', inject(function(interpolateFilter) {
-      expect(interpolateFilter('before %VERSION% after')).toEqual('before TEST_VER after');
+    it('should filter only the matching service', inject(function(searchServicesFilter) {
+        var matchingService = {'name':'test1', 'description': 'matching description'};
+        var matchedServices = [{'name':'test1',
+                                'description': 'matching description',
+                                'descriptionHl': '<strong>matching</strong> description',
+                                'found': true}];
+        var nonMatchingService = {'name':'test1', 'description': 'nothing to match'};
+        var services = [matchingService,nonMatchingService];
+        var q = 'matching';
+        expect(searchServicesFilter(services,q)).toEqual(matchedServices);
     }));
   });
 });
