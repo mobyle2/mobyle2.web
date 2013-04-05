9c73842a7e7ab21f1481f94443d2ae8bbcc8ef86
Hervé Ménager <hmenager@pasteur.fr>
2013-04-05 15:21:52 +0200

    added behavior tests for toggle directive and reformatted the code.

--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -2,31 +2,50 @@
 
 /* jasmine specs for directives go here */
 
-describe('directives', function() {
-  beforeEach(module('awa.directives'));
+describe('directives', function () {
+    beforeEach(module('awa.directives'));
 
-  describe('toggle', function(){
-    it('should display text-when-on if toggle-state==true', function(){
-      inject(function($compile, $rootScope) {
-        var element = $compile('<toggle text-when-on="when on" text-when-off="when off" toggle-state="1"></toggle>')($rootScope);
-        $rootScope.$digest();
-        expect(element[0].value).toEqual('when on');
-      });
-    });
-    it('should display text-when-off if toggle-state==false', function(){
-      inject(function($compile, $rootScope) {
-          var element = $compile('<toggle text-when-on="when on" text-when-off="when off" toggle-state="0"></toggle>')($rootScope);
-          $rootScope.$digest();
-          expect(element[0].value).toEqual('when off');
-      });
-    });
-    it('should display text-when-off if toggle-state is not defined', function(){
-      inject(function($compile, $rootScope) {
-          var element = $compile('<toggle text-when-on="when on" text-when-off="when off"></toggle>')($rootScope);
-          $rootScope.$digest();
-          expect(element[0].value).toEqual('when off');
-      });
-    });
-  })
-  
+    var scope;
+    var toggle_off, toggle_on, toggle_undef;
+
+    beforeEach(inject(function($rootScope, $compile){
+        toggle_on = angular.element('<toggle text-when-on="when on" text-when-off="when off" toggle-state="1"></toggle>');
+        toggle_off = angular.element('<toggle text-when-on="when on" text-when-off="when off" toggle-state="0"></toggle>');
+        toggle_undef = angular.element('<toggle text-when-on="when on" text-when-off="when off"></toggle>');
+        scope = $rootScope;
+        $compile(toggle_on)(scope);
+        $compile(toggle_off)(scope);
+        $compile(toggle_undef)(scope);
+        toggle_on = toggle_on[0];
+        toggle_off = toggle_off[0];
+        toggle_undef = toggle_undef[0];
+        scope.$digest();
+    }));
+
+
+
+
+    describe('toggle', function () {
+        it('should display text-when-on if toggle-state==true', function () {
+            expect(toggle_on.value).toEqual('when on');
+        });
+        it('should display text-when-off if toggle-state==false', function () {
+            expect(toggle_off.value).toEqual('when off');
+        });
+        it('should display text-when-off if toggle-state is not defined', function () {
+            expect(toggle_undef.value).toEqual('when off');
+        });
+        it('should display text-when-on if toggle-state==off and the element is clicked', function () {
+            toggle_off.click();
+            expect(toggle_off.value).toEqual('when off');
+        });
+        it('should display text-when-off if toggle-state==on and the element is clicked', function () {
+            // fails miserably on Chrome/Opera, but not on Firefox
+            // error message is: Uncaught Error: Non-assignable model expression: 0 (directive: toggle)
+            toggle_on.click();
+            expect(toggle_on.value).toEqual('when on');
+        });
+    })
+
 });
+
