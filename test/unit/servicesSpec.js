28c2af3ea10cf22fc35c19b5ac7aba020327ebc5
Hervé Ménager <hmenager@pasteur.fr>
2013-04-08 17:09:24 +0200

    more work on Angular service unit tests

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -13,6 +13,9 @@ describe('service', function() {
         it('should return false if the parameter has simple property set to false', inject(function(mbsimple) {
             expect(mbsimple({'simple':false})).toEqual(false);
         }));
+        it('should return false if nothing is provided', inject(function(mbsimple) {
+            expect(mbsimple()).toEqual(false);
+        }));
         it('should return false if the parameter has simple property unspecified', inject(function(mbsimple) {
             expect(mbsimple({})).toEqual(false);
         }));
@@ -23,4 +26,20 @@ describe('service', function() {
             expect(mbsimple({'children':[{'simple':true},{'simple':false}]})).toEqual(true);
         }));
     });
+/* work in progress...
+    describe('mfResource', function() {
+        var testMfResource, $httpBackend, scope;
+        beforeEach(inject(function($injector, mfResource) {
+            $httpBackend = $injector.get('$httpBackend');
+            $httpBackend.when('GET', '/api/test').respond([{id:1},{id:2}]);
+            scope = $rootScope.$new();
+            testMfResource = mfResource('test');
+        }));
+        it('should return a list of objects', inject(function() {
+           expect(testMfResource.query()).toEqual([{id:1},{id:2}]);
+           $httpBackend.flush();
+        }));
+     });
+*/
+
 });
