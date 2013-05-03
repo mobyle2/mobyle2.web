07a60229ac4e1aae417d252a9a58e6d6e44a07e2
Olivier Sallou <olivier.sallou@irisa.fr>
2013-05-03 15:01:24 +0200

    manage account login and registration via JSON for awa

--- mobyle/web/views.py
+++ mobyle/web/views.py
@@ -15,7 +15,11 @@ from mobyle.common import users
 from mobyle.common import service
 
 import urllib
+from urllib2 import URLError
 import urllib2
+
+import logging
+log = logging.getLogger(__name__)
 
 def add_user(user):
     """adds a user to the database. Password will be hashed with bcrypt"""
@@ -23,13 +27,31 @@ def add_user(user):
     user['hashed_password'] = hashed
     user.save()
 
+def create_if_no_exists(email, password=None):
+    """Check if user exists, else create it"""
+    user  = connection.User.find_one({'email': email})
+    if not user:
+        user= connection.User()
+        user['email'] = email
+        if password:
+            user['hashed_password'] = bcrypt.hashpw(password,
+            bcrypt.gensalt())
+        user.save()
+    return user
+
+
 
 def check_user_pw(username, password):
     """checks for plain password vs hashed password in database"""
+    if not password or password=='':
+        return None
     user  = connection.User.find_one({'email': username})
     if not user: return False
     hashed = bcrypt.hashpw(password, user['hashed_password'])
-    return hashed == user['hashed_password']
+    if hashed == user['hashed_password']:
+        return user
+    else:
+        return None
 
 
 @view_config(route_name='main', renderer='mobyle.web:templates/index.mako')
@@ -95,7 +117,8 @@ def login(request):
         username = request.POST['username']
         password = request.POST['password']
         
-        if check_user_pw(username, password):
+        user = check_user_pw(username, password)
+        if user:
             headers = remember(request, username)
             return HTTPFound(location="/", headers = headers)
     return Response("not logged in")
@@ -108,16 +131,44 @@ def logout(request):
     request.session.flash("You have logged out")
     return HTTPFound(location='/', headers=headers)
 
+
 @view_config(route_name="auth_login",renderer="json")
 def auth_login(request):
     # Needed for:
     # - Mozilla Persona
     # - Mobyle account
+    msg = ''
+    user = None
+    admin = False
     auth_system = request.matchdict['auth']
     settings = request.registry.settings
+    if auth_system == 'register':
+        ruser = request.json_body
+        user = ruser['username']
+        userobj = create_if_no_exists(ruser['username'],ruser['password'])
+        admin = False
+        status = 0
+        msg = "Your account is created, you can now log in the \
+        system with your new credentials"
+    if auth_system == 'native':
+        try:
+            ruser = request.json_body
+            userobj = check_user_pw(ruser['username'],
+                             ruser['password'])
+            if userobj:
+                status = 0
+                admin = userobj['admin']
+                user = ruser['username']
+            else:
+                status = 1
+                msg = "Wrong username or password"
+        except Exception:
+            user = None
+            status = 1
+            msg = "User does not exists"
     if auth_system == 'persona':
         assertion = request.params.getone('assertion')
-        audience = settings['site_uri']
+        audience = settings['site_awa']
         # Check assertion
         url = 'https://verifier.login.persona.org/verify'
         values = {'assertion': assertion,
@@ -132,20 +183,26 @@ def auth_login(request):
             response = urllib2.urlopen(req)
             auth_answer = response.read()
             verification_data = json.loads(auth_answer)
-            user = verification_data['email']
-            status = 0
+            if verification_data['status'] == 'failure':
+                status = 1
+            else:
+                user = verification_data['email']
+                status = 0
+                userobj = create_if_no_exists(user)
+                admin = userobj['admin']
         except URLError, e:
             logging.error(e.reason)
 
-    headers = remember(request, username)
-    request.response.headerlist.extend(headers)
-    return { 'user': user, 'status': status }
+    if user:
+        headers = remember(request, user)
+        request.response.headerlist.extend(headers)
+    return { 'user': user, 'status': status , 'msg': msg, 'admin' : admin}
 
 @view_config(route_name="auth_logout", renderer="json")
 def auth_logout(request):
     # logout
     headers = forget(request)
-    return { 'user': None, 'status': 0 }
+    return { 'user': None, 'status': 0 , 'msg' : ''}
 
 
 @view_config(route_name='services_list', renderer="json")
