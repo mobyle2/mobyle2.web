e63927fed35110782932a2a455ff9f17f9494bcf
Hervé Ménager <hmenager@pasteur.fr>
2013-09-19 09:48:22 +0200

    correct toggle directive unit tests

--- test/mobyle/test/unit/directivesSpec.js
+++ test/mobyle/test/unit/directivesSpec.js
@@ -12,10 +12,12 @@ describe('directives', function () {
         var toggle_off, toggle_on, toggle_undef;
 
         beforeEach(inject(function($rootScope, $compile){
-            toggle_on = angular.element('<toggle text-when-on="when on" text-when-off="when off" toggle-state="1"></toggle>');
-            toggle_off = angular.element('<toggle text-when-on="when on" text-when-off="when off" toggle-state="0"></toggle>');
+            toggle_on = angular.element('<toggle text-when-on="when on" text-when-off="when off" toggle-state="on"></toggle>');
+            toggle_off = angular.element('<toggle text-when-on="when on" text-when-off="when off" toggle-state="off"></toggle>');
             toggle_undef = angular.element('<toggle text-when-on="when on" text-when-off="when off"></toggle>');
             scope = $rootScope;
+            scope.on = true;
+            scope.off = false;
             $compile(toggle_on)(scope);
             $compile(toggle_off)(scope);
             $compile(toggle_undef)(scope);
@@ -39,14 +41,15 @@ describe('directives', function () {
 
         it('should display text-when-on if toggle-state==off and the element is clicked', function () {
             toggle_off.click();
-            expect(toggle_off.value).toEqual('when off');
+
+            expect(toggle_off.value).toEqual('when on');
         });
 
         it('should display text-when-off if toggle-state==on and the element is clicked', function () {
             // fails miserably on Chrome/Opera, but not on Firefox
             // error message is: Uncaught Error: Non-assignable model expression: 0 (directive: toggle)
             toggle_on.click();
-            expect(toggle_on.value).toEqual('when on');
+            expect(toggle_on.value).toEqual('when off');
         });
 
     });
